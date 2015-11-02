eede5758796fb93ca1279aaf4b7d98327d4e64b9
Mike Mueller <mikemueller04@hotmail.com>
2015-11-02 09:29:34 -0500

    add test secondsLampShouldBlinkEvery2Seconds

--- Repo/test/test/clockTest.java
+++ Repo/test/test/clockTest.java
@@ -2,13 +2,24 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.time.LocalTime;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 public class clockTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void secondsLampShouldBlinkEvery2Seconds() {
+		//arrange
+		
+		
+		//act
+		LocalTime localtime = LocalTime.of(0, 0, 0, 0);
+		String output = provideLamps(localtime);
+		
+		//assert
+		Assert.assertEquals("Y", output);
 	}
 
 }
