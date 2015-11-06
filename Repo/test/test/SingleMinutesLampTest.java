1b24a4478be19c7ae112f4c9139b58c969b17316
Mike Mueller <mikemueller04@hotmail.com>
2015-11-06 15:58:35 -0500

    failed test zeroMinutesRetrns00000

---
+++ Repo/test/test/SingleMinutesLampTest.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalTime;
+
+import src.Clock;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SingleMinutesLampTest {
+
+	@Test
+	public void zeroMinutesReturnsOOOO() {
+		//Arrange
+		Clock clock = new Clock();
+		
+		//Act
+		LocalTime time = LocalTime.of(0, 0, 0, 0);
+		String output = clock.provideLamps(time);
+		
+		//Assert
+		Assert.assertEquals("OOOO", output);
+	}
+
+}
