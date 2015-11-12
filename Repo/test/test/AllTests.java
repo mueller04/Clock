bb40567bde5b5120ecaad968e65300a408992849
Mike Mueller <mikemueller04@hotmail.com>
2015-11-12 11:19:06 -0500

    fail test provideFiveMinutesLamp

---
+++ Repo/test/test/AllTests.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SecondsLampTest.class, SingleMinutesLampTest.class })
+public class AllTests {
+
+}
